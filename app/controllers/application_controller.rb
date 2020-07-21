2277d9e17f7621e2e37e2fa4204ecb858d3eec88
Ketty Lezama <ketty.lezama9@gmail.com>
2020-07-21 17:28:49 -0400

    WIP - added username column
    Co-authored-by: LegioTitanicus <alexbernstein7170@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
+
+  protected
+  def configure_permitted_parameters
+    binding.pry
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :username, :password])
+
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:username, :password])
+  end
 end
