81cf0cf38aa0807c42fd95c82545251c5973fc90
Ketty Lezama <ketty.lezama9@gmail.com>
2020-07-22 10:30:01 -0400

    Finished writing and passing spec tests for User.
    Co-authored-by: Kajsa Senchuk <kajsa.senchuk@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,6 @@ class ApplicationController < ActionController::Base
 
   protected
   def configure_permitted_parameters
-    binding.pry
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :username, :password])
 
     devise_parameter_sanitizer.permit(:sign_in, keys: [:username, :password])
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true
+  validates :username, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
 end
