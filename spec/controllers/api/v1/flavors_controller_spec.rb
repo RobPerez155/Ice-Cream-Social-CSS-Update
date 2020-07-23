b7e707c3f2a2766aaa171611bf8e58b00a2fad3d
B-rye <brianmanhard@gmail.com>
2020-07-23 15:11:22 -0400

    WIP - Corrected rspec tests for Flavor controller, awaiting Region Show page work to be finalized before continuing

--- spec/controllers/api/v1/flavors_controller_spec.rb
+++ spec/controllers/api/v1/flavors_controller_spec.rb
@@ -1,17 +1,21 @@
 require "rails_helper"
 
 RSpec.describe Api::V1::FlavorsController, type: :controller do
+  let!(:flavor_test_region) { Region.create(
+    name: "Flavor Town",
+    image_url: "https://www.google.com"
+  ) }
   let!(:first_flavor) { Flavor.create(
     name: "Test Flavor 1",
     image_url: "https://www.google.com",
     description: "Tastes like test flavor 1, yummy!",
-    region_id: "1"
+    region: flavor_test_region
   ) }
   let!(:second_flavor) { Flavor.create(
     name: "Test Flavor 2",
     image_url: "https://www.google.com",
     description: "Tastes like test flavor 2, yummy!",
-    region_id: "2"
+    region: flavor_test_region
   ) }
 
   describe "GET#index" do
@@ -27,12 +31,10 @@ RSpec.describe Api::V1::FlavorsController, type: :controller do
 
         expect(returned_json[0]["name"]).to eq "Test Flavor 1"
         expect(returned_json[0]["image_url"]).to eq "https://www.google.com"
-        expect(returned_json[0]["region_id"]).to eq "1"
 
         expect(returned_json.length).to eq 2
         expect(returned_json[1]["name"]).to eq "Test Flavor 2"
         expect(returned_json[1]["image_url"]).to eq "https://www.google.com"
-        expect(returned_json[1]["region_id"]).to eq "2"
 
     end
   end
